1b600c5b0dca457e27a087a9d38e541276ed796c
root <ring3367@naver.com>
2022-09-14 17:46:33 +0900

    Geting Username

--- potato.js
+++ potato.js
@@ -2,13 +2,19 @@
 //const loginInput = loginForm.querySelector("input");
 //const loginButton = loginForm.querySelector("button");
 
+const greeting = document.querySelector("#greeting");
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 //const loginButton = document.querySelector("#login-form button");
 
 function handleOnSubmit(submitEvent) {
   submitEvent.preventDefault(); // 브라우저의 기본 동작을 막아줌
-  console.log(loginInput.value);
+  const username = loginInput.value;
+  const HIDDEN_CLASSNAME = "hidden";
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${username}`;
+  console.log(username);
 }
 
 loginForm.addEventListener("submit", handleOnSubmit);
